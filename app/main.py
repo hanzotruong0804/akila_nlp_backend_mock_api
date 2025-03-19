42195c7d15472967ff136df4d02c85e2e26189ec
Harry Truong <harry.truong@akila3d.com>
2025-03-19 17:12:39 +0700

    fix CORS

--- app/main.py
+++ app/main.py
@@ -1,8 +1,17 @@
-
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from app.routers import chat
 
 app = FastAPI(title="Akila NLP backend Chat Mock API")
+
+# Configure CORS
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allows all methods
+    allow_headers=["*"],  # Allows all headers
+)
 
 # Đăng ký router chat
 app.include_router(chat.router)
